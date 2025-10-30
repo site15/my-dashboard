219913e4f72c288821e846b06bd6ce589977f4de
EndyKaufman <admin@site15.ru>
2025-10-30 06:35:24 +0500

    feat: добавил сайт

---
+++ web/vite.config.ts
@@ -0,0 +1,33 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite';
+import analog from '@analogjs/platform';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  build: {
+    target: ['es2020'],
+  },
+  resolve: {
+    mainFields: ['module'],
+  },
+  plugins: [
+    analog({
+      nitro: {
+        preset: 'vercel',
+      },
+    }),
+    tailwindcss()
+  ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['src/test-setup.ts'],
+    include: ['**/*.spec.ts'],
+    reporters: ['default'],
+  },
+  define: {
+    'import.meta.vitest': mode !== 'production',
+  },
+}));
