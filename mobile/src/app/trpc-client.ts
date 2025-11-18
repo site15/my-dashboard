51b78e1c744e4ddd432868028ca6fba0f4647d5d
EndyKaufman <admin@site15.ru>
2025-11-19 00:31:10 +0500

    fix(trpc-client): enhance customFetch function for server and client
    - Add isomorphic-fetch import for universal fetch support
    - Implement custom fetch logic to use global $fetch if available
    - Include credentials and headers in fetch requests for CORS with cookies
    - Handle server-side requests with dynamic host and port environment variables
    - Adjust input to Request or URL with base URL in server environments
    - Apply changes to both mobile and web trpc-client modules

--- mobile/src/app/trpc-client.ts
+++ mobile/src/app/trpc-client.ts
@@ -1,11 +1,47 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { createTrpcClient } from '@analogjs/trpc';
 import { inject } from '@angular/core';
-
+import 'isomorphic-fetch';
 import { AppRouter } from '../../../web/src/server/trpc/routers/index';
 import { environment } from '../environments/environment';
 
-function customFetch(input: RequestInfo, init?: RequestInit) {
+function customFetch(
+  input: RequestInfo | URL,
+  init?: RequestInit & { method: 'GET' }
+) {
+  if ((globalThis as any).$fetch) {
+    return (globalThis as any).$fetch
+      .raw(input.toString(), {
+        ...init,
+        credentials: 'include', // важно для CORS с куки
+        headers: {
+          ...(init?.headers || {}),
+        },
+      })
+      .catch((e: any) => {
+        throw e;
+      })
+      .then((response: any) => ({
+        ...response,
+        headers: response.headers,
+        json: () => Promise.resolve(response._data),
+      }));
+  }
+
+  // dev server trpc for analog & nitro
+  if (typeof window === 'undefined') {
+    const host =
+      process.env['NITRO_HOST'] ?? process.env['ANALOG_HOST'] ?? 'localhost';
+    const port =
+      process.env['NITRO_PORT'] ?? process.env['ANALOG_PORT'] ?? 4205;
+    const base = `http://${host}:${port}`;
+    if (input instanceof Request) {
+      input = new Request(base, input);
+    } else {
+      input = new URL(input, base);
+    }
+  }
+
   return fetch(input, {
     ...init,
     credentials: 'include', // важно для CORS с куки
